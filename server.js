53c64fe8023832796c6a397d85825ea11a1a8a67
swiatekmateusz <swiatek.mateusz.m@gmail.com>
2019-12-27 23:49:48 +0100

    Prepare for deploy

--- server.js
+++ server.js
@@ -12,6 +12,13 @@ app.use('/api/auth', require('./routes/auth'))
 app.use('/api/email', require('./routes/email'))
 app.use('/api/password', require('./routes/password'))
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT)
